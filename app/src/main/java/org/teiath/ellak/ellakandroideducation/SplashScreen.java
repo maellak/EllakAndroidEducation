d0968209968e780daecf0ef1c12d193f6e4e006d
DimKon7 <ee06155@teiath.gr>
2015-09-27 23:30:31 +0300

    Splash & Option Screen

--- app/src/main/java/org/teiath/ellak/ellakandroideducation/SplashScreen.java
+++ app/src/main/java/org/teiath/ellak/ellakandroideducation/SplashScreen.java
@@ -1,7 +1,18 @@
 package org.teiath.ellak.ellakandroideducation;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 /**
  * Αποτελεί την αρχική οθόνη της εφαρμογής. Παρουσιάζει τα στοιχεία του project (ΕΛ/ΛΑΚ, τίτλος εφαρμογής, κλπ).
@@ -12,12 +23,94 @@ import android.os.Bundle;
  */
 public class SplashScreen extends Activity
 {
+    private final int SPLASH_DISPLAY_LENGTH = 3000;
+    private UpdateHandler UpdateHandler;
+    private DBHandler DBHandler;
 
     @Override
     protected void onCreate (Bundle savedInstanceState)
     {
-        super.onCreate (savedInstanceState);
-        setContentView (R.layout.lay_splash);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lay_splash);
+
+        UpdateHandler= new UpdateHandler(this);
+        DBHandler = new DBHandler(this);
+
+        float db = DBHandler.GetVersion();
+
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+
+        if(activeNetworkInfo != null && activeNetworkInfo.isConnected()){
+
+            float Newdb = UpdateHandler.GetVersion();
+            if(db != Newdb) {
+                new AlertDialog.Builder(SplashScreen.this)
+                        .setTitle(R.string.choice)
+                        .setMessage(R.string.newdb)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                LoadNewDb start = new LoadNewDb();
+                                start.execute();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                Intent mainIntent = new Intent(SplashScreen.this, OptionScreen.class);
+                                SplashScreen.this.startActivity(mainIntent);
+                                SplashScreen.this.finish();
+                            }
+                        })
+                        .setIcon(android.R.drawable.ic_dialog_info)
+                        .show();
+
+            }else{
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+
+                        Intent mainIntent = new Intent(SplashScreen.this, OptionScreen.class);
+                        SplashScreen.this.startActivity(mainIntent);
+                        SplashScreen.this.finish();
+                    }
+                }, SPLASH_DISPLAY_LENGTH);
+            }
+        }
+
+        else {
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+
+                    Intent mainIntent = new Intent(SplashScreen.this, OptionScreen.class);
+                    SplashScreen.this.startActivity(mainIntent);
+                    SplashScreen.this.finish();
+                }
+            }, SPLASH_DISPLAY_LENGTH);
+        }
+    }
+
+    class LoadNewDb extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... params) {
+            UpdateHandler.GetNewDB();
+            return "finished";
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+            Toast.makeText(getApplicationContext(), "Η βάση δεδομένων ανανεώνεται, παρακαλώ περιμένετε ...", Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Intent mainIntent = new Intent(SplashScreen.this, OptionScreen.class);
+            SplashScreen.this.startActivity(mainIntent);
+            SplashScreen.this.finish();
+        }
     }
 
 }
